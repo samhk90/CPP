66b39fdb907d7990d0532aaf93249c330d1aa6a3
Mohammed Samee <86250719+samhk90@users.noreply.github.com>
2022-12-04 12:18:09 +0530

    Add files via upload

---
+++ publication.cpp
@@ -0,0 +1,162 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+class publication
+{
+   
+
+public:
+ string ttitle,btitle;
+    float tprice,bprice;
+    void bgetinfo()
+    {
+        cout << "\nEnter the Title:: ";
+        cin.ignore();
+        getline(cin,btitle);
+        cout << "\nEnter the Price:: ";
+        cin >> bprice;
+    }
+    void bputinfo()
+    {
+        cout << "\nTitle of Book:: " << btitle;
+        cout << "\nPrice of Book:: " << bprice;
+    }
+    void tgetinfo()
+    {
+        cout << "\nEnter the Title:: ";
+        cin.ignore();
+        getline(cin,ttitle);
+        cout << "\nEnter the Price:: ";
+        cin >> tprice;
+    }
+    void tputinfo()
+    {
+        cout << "\nTitle of Book:: " << ttitle;
+        cout << "\nPrice of Book:: " << tprice;
+    }
+};
+class book : public publication
+{
+public:
+int page;
+    void getbook()
+    {
+        bgetinfo();
+        cout << "\nEnter the number of pages:: ";
+        cin >> page;
+        try{
+            if (page<500){
+			
+			throw(page);
+			}
+        }catch(int)
+        {
+            btitle="0";
+            page=0;
+            bprice=0.0;
+            cout<<"\nInvalid Data";
+            putbook();
+        }
+    }
+    void putbook()
+    {
+       bputinfo();
+    	cout<<"\nPage count:: "<<page; 
+    }
+};
+class tape : public publication
+{
+public:
+ float time;
+    void gettape()
+    {
+        tgetinfo();
+        cout << "\nEnter play Time in minutes";
+        cin >> time;
+        try{
+            if (time<30.02)
+            {
+			throw(time);
+		}
+        }
+        catch(float)
+        {
+            ttitle="0";
+            time=0.0;
+            tprice=0.0;
+            cout<<"\nInvalid Data";
+            puttape();
+        }
+    }
+    void puttape()
+    {
+    	tputinfo();
+            cout<<"\nTime period:: "<<time;
+       
+    }
+};
+int main()
+{
+    book b[100];
+    tape t[100];
+    int ch, bcount = 0, tcount = 0;
+
+  do{
+        cout << "\n****************PUBLICATION***************";
+        cout << "\n1.Add Book Info";
+        cout << "\n2.Add Tape Info";
+        cout << "\n3.Display Book Info";
+        cout << "\n4.Display Tape Info";
+        cout << "\n5.EXIT";
+        cout << "\nEnter choice:: ";
+        cin >> ch;
+        switch (ch)
+        {
+        case 1:
+        {
+            cout << "\n****Add Book****";
+            b[bcount].getbook();
+            bcount++;
+            break;
+        }
+        case 2:
+        {
+            cout << "\n****Add Tape****";
+            t[tcount].gettape();
+            tcount++;
+            break;
+        }
+        case 3:
+        {
+            cout << "\n****************PUBLICATION***************";
+            for (int i = 0; i < bcount;i++)
+            {
+                b[i].putbook();                
+            }
+            break;
+        }
+        case 4:
+        {
+            cout << "\n****************PUBLICATION***************";
+            for (int j = 0; j < tcount; j++)
+            {
+                t[j].puttape();
+                
+            }
+			break;
+        }
+        case 5:
+        {
+            cout << "\nThank you";
+            break;
+        }
+        default:
+        {
+            cout << "\nInvalid Option";
+            break;
+        }
+        }
+    } while (ch != 5);
+    getch();
+    return 0;
+}
